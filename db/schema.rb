495a1dceca5534fdacca091370fb946ada3e8550
Matias Bensan Araya <mbensan@gmail.com>
2024-11-29 21:26:30 -0300

    Se añade vista de todos los prpductos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_29_002407) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_30_001211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_29_002407) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stock", default: 0, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
